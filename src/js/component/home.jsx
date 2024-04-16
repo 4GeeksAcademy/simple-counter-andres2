35ea005ef17461957bbc0874d9ffa60eacf3ceb4
Andrés Torres <125112555+andres-stg@users.noreply.github.com>
2024-04-16 06:31:24 +0000

    basecode

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,36 +1,37 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const counter = () => {
-    const [segundos, setSeconds] = useState (0);
-    const [digitos, setDigits] = useState ([0,0,0,0,0,0]);
+const Counter = () => {
+    const [seconds, setSeconds] = useState(0);
+    const [digits, setDigits] = useState([0, 0, 0, 0, 0, 0]);
 
-    useEffect (() => {
+    useEffect(() => {
         const interval = setInterval(() => {
-            const digitsArray = seconds.toString().padStart(6,'0').split('');
-                setDigits(digitsAray);
-                setSeconds ((prevSeconds) => prevSeconds +1);
+            const digitsArray = seconds.toString().padStart(6, '0').split('');
+            setDigits(digitsArray);
+            setSeconds((prevSeconds) => prevSeconds + 1);
         }, 1000);
 
-    return () => clearInterval(interval);
-}, [seconds]);
+        return () => clearInterval(interval);
+    }, [seconds]);
 
-return (
-    <div className='numberedCard'>
+    return (
+        <div className='numberedCard container-fluid'>
 
-        <H5>{digits[0]}</H5>
-        <H5>{digits[0]}</H5>
-        <H5>{digits[0]}</H5>
-        <H5>{digits[0]}</H5>
-        <H5>{digits[0]}</H5>
-        <H5>{digits[0]}</H5>
+            <h5>{digits[0]}</h5>
+            <h5>{digits[0]}</h5>
+            <h5>{digits[0]}</h5>
+            <h5>{digits[0]}</h5>
+            <h5>{digits[0]}</h5>
+            <h5>{digits[0]}</h5>
 
-    </div>
-  );
+        </div>
+    );
 };
 
 
-export default counter;
+export default Counter;
